fa7a4e910d1c02376dcf39ebc166316c7c92f24a
Maxime Menant <darklaza@gmail.com>
2012-01-16 21:05:55 +0100

    Add model User and Home Controller
    Add the devise model User
    Add the root controller: Home

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+#
+# Copyright ©2010-2012 Maxime Menant
+#
+# Created by: mmenant
+# Created on: 16/01/12
+# 
+
+
+class HomeController < ApplicationController
+  # To change this template use File | Settings | File Templates.
+
+  def index
+
+  end
+
+end
