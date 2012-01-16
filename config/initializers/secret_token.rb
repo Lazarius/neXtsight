f1621a0fea8fb849e3245e733620e43aa649ed37
Maxime Menant <darklaza@gmail.com>
2012-01-16 20:58:20 +0100

    Create the RoR Project for neXtSight
    Set the project, config to use MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeXtSight::Application.config.secret_token = '8338e03fff0f13f66d0b80f136b7c2ecabcb59317588e7dac9696199d46a048fb9f078467a4e266e7a281dac6d4ec52e2510bbb768973a873e259379af5b07bc'
